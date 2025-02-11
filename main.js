7b9d5f72647dca495e969691c44aeff8ba6849d6
Sabuj Islam <sabuj0338@gmail.com>
2025-02-12 00:38:45 +0600

    updates

--- main.js
+++ main.js
@@ -99,21 +99,22 @@ io.on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
+      console.log("User disconnected:", roomId, socket.id);
       users[roomId] = users[roomId].filter((id) => id !== socket.id);
       io.to(roomId).emit("room:user-left", socket.id);
     });
   });
 
-  // socket.on("disconnect", () => {
-  //   console.log("User disconnected:", socket.id);
-  //   const roomId = Object.keys(users).find((roomId) =>
-  //     users[roomId].includes(socket.id)
-  //   );
-  //   if (roomId) {
-  //     users[roomId] = users[roomId].filter((id) => id !== socket.id);
-  //     io.to(roomId).emit("room:user-left", socket.id);
-  //   }
-  // });
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    const roomId = Object.keys(users).find((roomId) =>
+      users[roomId].includes(socket.id)
+    );
+    if (roomId) {
+      users[roomId] = users[roomId].filter((id) => id !== socket.id);
+      io.to(roomId).emit("room:user-left", socket.id);
+    }
+  });
 });
 
 server.listen(3000, () => {
