55c7066f89073805fdc87378b6bd7125115dd258
Sabuj Islam <sabuj0338@gmail.com>
2025-02-11 19:09:15 +0600

    room connection added

--- main.js
+++ main.js
@@ -21,39 +21,86 @@ app.use(
   })
 );
 
+const users = {}; // Store connected users
+
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  socket.on("toggle-video", (data) => {
-    socket.broadcast.emit("toggle-video", data);
-  });
-  
-  socket.on("toggle-audio", (data) => {
-    socket.broadcast.emit("toggle-audio", data);
-  });
-  
-  socket.on("screen-share", (data) => {
-    socket.broadcast.emit("screen-share", data);
-  });
+  socket.on("join-room", (roomId) => {
+    if (!users[roomId]) users[roomId] = [];
+    users[roomId].push(socket.id);
 
-  socket.on("offer", (data) => {
-    socket.broadcast.emit("offer", data);
-  });
+    // Notify existing users
+    users[roomId].forEach((peerId) => {
+      if (peerId !== socket.id) {
+        io.to(peerId).emit("user-joined", socket.id);
+      }
+    });
 
-  socket.on("answer", (data) => {
-    socket.broadcast.emit("answer", data);
-  });
+    socket.on("toggle-video", (peerId, data) => {
+      io.to(peerId).emit("toggle-video", socket.id, data);
+    });
 
-  socket.on("ice-candidate", (data) => {
-    socket.broadcast.emit("ice-candidate", data);
-  });
+    socket.on("toggle-audio", (peerId, data) => {
+      io.to(peerId).emit("toggle-audio", socket.id, data);
+    });
 
-  socket.on("hang-up", (data) => {
-    socket.broadcast.emit("hang-up", data);
-  });
+    socket.on("screen-share", (peerId, data) => {
+      io.to(peerId).emit("screen-share", socket.id, data);
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    socket.on("hang-up", (peerId, data) => {
+      io.to(peerId).emit("hang-up", socket.id, data);
+    });
+
+    socket.on("offer", (peerId, offer) => {
+      io.to(peerId).emit("offer", socket.id, offer);
+    });
+
+    socket.on("answer", (peerId, answer) => {
+      io.to(peerId).emit("answer", socket.id, answer);
+    });
+
+    socket.on("ice-candidate", (peerId, candidate) => {
+      io.to(peerId).emit("ice-candidate", socket.id, candidate);
+    });
+
+    socket.on("disconnect", () => {
+      users[roomId] = users[roomId].filter((id) => id !== socket.id);
+      io.to(roomId).emit("user-left", socket.id);
+    });
+
+    // socket.on("toggle-video", (data) => {
+    //   socket.broadcast.emit("toggle-video", data);
+    // });
+
+    // socket.on("toggle-audio", (data) => {
+    //   socket.broadcast.emit("toggle-audio", data);
+    // });
+
+    // socket.on("screen-share", (data) => {
+    //   socket.broadcast.emit("screen-share", data);
+    // });
+
+    // socket.on("hang-up", (data) => {
+    //   socket.broadcast.emit("hang-up", data);
+    // });
+
+    // socket.on("offer", (data) => {
+    //   socket.broadcast.emit("offer", data);
+    // });
+
+    // socket.on("answer", (data) => {
+    //   socket.broadcast.emit("answer", data);
+    // });
+
+    // socket.on("ice-candidate", (data) => {
+    //   socket.broadcast.emit("ice-candidate", data);
+    // });
+
+    // socket.on("disconnect", () => {
+    //   console.log("User disconnected:", socket.id);
+    // });
   });
 });
 
