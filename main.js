1d4dece70d5c9a264eb9622bd9028c1bfe2b9a18
Sabuj Islam <sabuj0338@gmail.com>
2025-02-12 10:28:35 +0600

    extra disconnect method removed

--- main.js
+++ main.js
@@ -104,17 +104,6 @@ io.on("connection", (socket) => {
       io.to(roomId).emit("room:user-left", socket.id);
     });
   });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-    const roomId = Object.keys(users).find((roomId) =>
-      users[roomId].includes(socket.id)
-    );
-    if (roomId) {
-      users[roomId] = users[roomId].filter((id) => id !== socket.id);
-      io.to(roomId).emit("room:user-left", socket.id);
-    }
-  });
 });
 
 server.listen(3000, () => {
