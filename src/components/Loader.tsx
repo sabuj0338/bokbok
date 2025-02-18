49236a8ada9edef4d0bb4e52f323ff5b2794e720
Sabuj Islam <sabuj0338@gmail.com>
2025-02-18 09:10:21 +0600

    loader color fixed

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ export default function Loader() {
   return (
     <div className="min-h-screen w-full h-full flex items-center justify-center">
       <svg
-        className="animate-spin h-6 w-6 text-white"
+        className="animate-spin h-6 w-6 text-zinc-900 dark:text-white"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
