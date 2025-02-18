03f370d0ff6961c959ce652cd3f4e56d2b75975f
Sabuj Islam <sabuj0338@gmail.com>
2025-02-18 20:35:55 +0600

    added domain to cors

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://bokbok-chi.vercel.app"],
+    origin: ["http://localhost:5173", "https://bokbok-chi.vercel.app", "https://alap2.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bokbok-chi.vercel.app"],
+    origin: ["http://localhost:5173", "https://bokbok-chi.vercel.app", "https://alap2.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   })
